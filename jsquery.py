da8a3fc2affc249433b0188c4d07e5348b31e98d
Timmy <zhu327@gmail.com>
2013-09-25 20:48:09 +0800

    add real time interface

--- jsquery.py
+++ jsquery.py
@@ -33,3 +33,24 @@ def query(code):
         return dict(code=v1[0][13:], name=v2[0].decode('gb2312'),
                    newnet=v2[1],
                    daygrowrate=('%.2f%%' % ( 100*(float(v2[1])-float(v2[3]))/float(v2[3]) )))
+
+def queryReal(codelist):
+    clist = ['fu_%s' % c for c in codelist]
+    url = 'http://hq.sinajs.cn/list=%s' % ','.join(clist)
+    response = urllib2.urlopen(url)
+    var = response.read()
+    vlist = var.split('";\n')
+    result = list()
+    for v in vlist:
+        if v:
+            v1 = v.split('="')
+            if v1[1]:
+                v2 = v1[1].split(',')
+                cdict = dict(code=v1[0][14:], name=v2[0].decode('gb2312'),
+                            newnet=v2[2],
+                            daygrowrate=('%.2f%%' % float(v2[6])))
+                result.append(cdict)
+            else:
+                pass
+    return result
+
